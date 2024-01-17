8ec5c0af8e4a09f04479e9048aa8fcfd20c2ee4f
Nasir <83594538+omersterlinghoffman@users.noreply.github.com>
2024-01-17 21:09:30 +0500

    add

--- refernce.php
+++ refernce.php
@@ -1,3 +1,6 @@
 https://github.com/fintech07/stripe-payment-management-terminal
 https://github.com/topics/stripe-subscriptions?o=asc&s=stars
 https://github.com/Rahul-Barui/Stripe-Payment-Gateway-Integration-in-PHP/blob/master/index.php
+https://designmodo.static.app/bootstrap5form/index.html
+https://w3sniff.com/user/guest/post-details/60/Multi-Step-Form-with-Progress-Bar-using-bootstrap-5
+http://frontendfreecode.com/bootstrap-signup-form-wizard-with-validation
